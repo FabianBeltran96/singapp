9a03ba6bd3c58335dca841e237bbf363dea74f13
Fabian Esteban Beltran Gomez <fabianbeltran1224@gmail.com>
2025-01-08 11:11:17 -0500

    adjust layout and adding popup confirmation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,53 +1,32 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-
 import Entypo from '@expo/vector-icons/Entypo';
-import AntDesign from '@expo/vector-icons/AntDesign';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            position: 'absolute',
-          },
-          default: {},
-        }),
+        tabBarActiveTintColor: '#2196F3',
+        headerShown: true,
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => <AntDesign name="home" size={24} color={focused ? 'black' : 'gray'} />,
-        }}
-      />
-      <Tabs.Screen
-        name="list"
-        options={{
-          title: 'Listado',
-          tabBarIcon: ({ color, focused }) => <Entypo name="list" size={24} color={focused ? 'black' : 'gray'} />,
+          title: 'Servicios',
+          headerTitle: 'Mis Servicios',
+          tabBarIcon: ({ color, focused }) => (
+            <Entypo name="list" size={24} color={focused ? '#2196F3' : 'gray'} />
+          ),
         }}
       />
       <Tabs.Screen
         name="filed"
         options={{
-          title: 'Archivado',
-          tabBarIcon: ({ color, focused }) => <Ionicons name="file-tray" size={24} color={focused ? 'black' : 'gray'} />,
+          title: 'Firmados',
+          headerTitle: 'Firmados',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name="file-tray" size={24} color={focused ? '#2196F3' : 'gray'} />
+          ),
         }}
       />
     </Tabs>
